4b790e6701002b13055bf0a9815c1553bf009630
LouisHyt <louishayotte@gmail.com>
2023-06-27 16:18:42 +0200

    Adding functions to organize the code

--- script.js
+++ script.js
@@ -1,14 +1,58 @@
 const board = document.querySelector("#board");
+const digits = document.querySelector("#digits");
 const box = document.createElement("div");
 box.classList.add("box");
 
+
+const arrayScore = JSON.parse(localStorage.getItem("scores")) || [];
+
+let centi = 0;
+let dixi = 0;
+let sec = 0;
+let min = 0;
+let secFormat;
+let score;
+let chronoTimeout;
+
+displayScore();
+
 startGame();
+
+function chrono(){
+    //Centièmes de secondes
+    digits.classList.remove("score-result");
+    setInterval(() => {
+        centi++;
+        if(centi > 9) {
+            centi = 0;
+        }
+    }, 10)
+
+    //Dixieme de secondes
+    dixi++;
+    dixi*10;
+    if(dixi > 9) {
+        dixi = 0;
+        sec++
+    }
+
+    //Secondes
+    if(sec < 10){
+        secFormat = "0" + sec;
+    } else {
+        secFormat = sec;
+    }
+
+    score = secFormat + dixi + centi;
+    digits.textContent = score;
+    //Réexecute tous les dixieme de seconde
+    chronoTimeout = setTimeout(chrono, 100)
+}
 
 function startGame(){
     nbBoites = prompt("Veuillez insérer le nombre de boite souhaité !") || 10;
-
+    chrono();
     let nb = 1;
-    let timer = 0; 
     for(let i = 1; i <= nbBoites; i++){
         let newBox = box.cloneNode();
         newBox.textContent = i;
@@ -19,9 +63,7 @@ function startGame(){
                 case i == nb :
                     this.classList.add("box-clicked");
                     if(nb == board.children.length){
-                        board.querySelectorAll(".box").forEach(elem => {
-                            showReaction("success", elem);
-                        })
+                        handleEndGame();
                     }
                     shuffleChildren(board);
                     nb++;
@@ -42,10 +84,22 @@ function startGame(){
     }
     
     shuffleChildren(board)
-    let timerInterval = setInterval(() => {
-        timer++;
-        
-    }, 1000)
+}
+
+function handleEndGame(){
+    clearTimeout(chronoTimeout);
+    digits.classList.add("score-result");
+    board.querySelectorAll(".box").forEach(elem => {
+        showReaction("success", elem);
+    })
+
+    arrayScore.push(parseInt(score));
+    arrayScore.sort((a, b) => {
+        return a - b
+    })
+    if(arrayScore.length > 3) arrayScore.pop();
+    localStorage.setItem("scores", JSON.stringify(arrayScore));
+    displayScore();
 }
 
 function shuffleChildren(parent){
@@ -69,4 +123,15 @@ function showReaction(type, clickedBox){
             clickedBox.classList.remove(type)
         }, 800) 
     }
+}
+
+function displayScore(){
+    for(let i = 0; i < 3; i++){
+        if(!arrayScore[i]){
+            document.querySelector(`.score${i + 1}`).textContent = "N°" +i+ " : " + "Indéfini";
+        } else {
+            document.querySelector(`.score${i + 1}`).textContent = "N°" +i+ " : " + arrayScore[i];
+            
+        }
+    }5
 }
